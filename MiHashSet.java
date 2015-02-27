9b9916d8459fd526a7138c3ec172a2603e92f68f
BuildTools <unconfigured@null.spigotmc.org>
2015-02-27 22:12:57 +0100

    primer commit

---
+++ MiHashSet.java
@@ -0,0 +1,15 @@
+
+/**
+ * Clase que emula todos los métodos de la clase HashSet sin el uso de la misma.
+ * @author (Josu) 
+ * @version (28/02/2015)
+ */
+public class MiHashSet {
+    
+    /**
+     * Constructor for objects of class MiHashSet
+     */
+    public MiHashSet() {
+      
+    }
+}
